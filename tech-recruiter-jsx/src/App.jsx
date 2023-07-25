3134955533dac613d6a118983158fccc39699974
Samuel Paixão Nascimento <samdeveloperbr@gmail.com>
2023-07-25 20:02:17 +0000

    Adição do footer e modificações de css

--- tech-recruiter-jsx/src/App.jsx
+++ tech-recruiter-jsx/src/App.jsx
@@ -5,14 +5,20 @@ import InputLog from './components/inputs/inputLog'
 import Button from "./components/buttons/buttons"
 function App() {
 
-  return (
+  return (<>
     <div className='app'>
+      <span className='logo'>ATS</span>
+      <div className='container' style={{textAlign: "center"}}>
       <div className='container-login'>
-        <InputLog type="text" placeholder="Email"/>
-        <InputLog type="password" placeholder="Digite a sua senha"/>
+          <InputLog type="text" placeholder="Email"/>
+          <InputLog type="password" placeholder="Digite a sua senha"/>
+        </div>
+        <Button className="submit-form">Entrar</Button>
+        <p style={{color: "var(--gray-53)", marginTop: "0.5625rem"}}>Esqueceu a senha?</p>
       </div>
-      <Button className="submit-form">Entrar</Button>
     </div>
+        <div className='styled-footer'></div>
+        </>
   )
 }
 
