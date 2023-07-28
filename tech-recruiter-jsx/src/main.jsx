d5d45e74e91a947e419305deef611b7447c01a7f
Samuel Paixão Nascimento <samdeveloperbr@gmail.com>
2023-07-28 13:13:58 +0000

    Globalstyle do styled components

--- tech-recruiter-jsx/src/main.jsx
+++ tech-recruiter-jsx/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import LightTheme from './components/global/LightTheme.js'
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+      <LightTheme/>
       <App />
   </React.StrictMode>,
 )
